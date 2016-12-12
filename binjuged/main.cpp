1a50686aa591a59a1ccebe2d7071e9bebf9f4187
Nicholas Buckeridge <bucknich@gmail.com>
2016-12-12 14:43:30 +1000

    Initial Commit

---
+++ binjuged/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  binjuged
+//
+//  Created by Nicholas Buckeridge on 12/12/16.
+//  Copyright © 2016 Nicholas Buckerdige. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
